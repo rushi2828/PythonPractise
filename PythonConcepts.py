7fdb9a794ed81d287d151ee254c02e3f1f032a87
rushi2828 <rushi2828@gmail.com>
2019-09-20 16:53:06 +0530

    First Python code

---
+++ PythonConcepts.py
@@ -0,0 +1,16 @@
+# Python Strings
+from builtins import len
+
+test = "This is Python First"
+
+print('o/p is ----> ', test)  # o/p--->i
+
+# Print particular character
+print('o/p is ----> ', test[2])  # o/p--->This is Python First
+
+# Print from particular character
+print('o/p is ----> ', test[8:])  # o/p---> Python First
+
+print(len(test))
+
+print (test.digits())
