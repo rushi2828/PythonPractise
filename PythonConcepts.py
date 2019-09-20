6f29669a1c8a7b07c9f4d9c78d0b6acdae3ca733
rushi2828 <rushi2828@gmail.com>
2019-09-20 20:12:58 +0530

    Added string operation/methods

--- PythonConcepts.py
+++ PythonConcepts.py
@@ -1,7 +1,7 @@
 # Python Strings
-from builtins import len
+from builtins import len, input, int
 
-test = "This is Python First"
+test = "this is Python First"
 
 print('o/p is ----> ', test)  # o/p--->i
 
@@ -13,4 +13,53 @@ print('o/p is ----> ', test[8:])  # o/p---> Python First
 
 print(len(test))
 
-print (test.digits())
+print(test.upper())         # o/p---> THIS IS PYTHON FIRST
+
+print(test.lower())         # o/p---> this is python first
+
+print(test.capitalize())    # o/p---> This is python first
+# ====================================================================
+'''
+Multiple lines Comments
+Like this
+'''
+
+# ====================================================================
+print('===================================')
+# If-else statements
+
+'''age = int(input("Enter the age"))
+
+if age < 18:
+    print('You are child')
+else:
+    print('Congrats You Are Adult Now')
+
+# Example No. : 2
+
+num = int(input('Enter the value'))
+if num % 2 == 0:
+    print ('Even')
+else:
+    print('Odd')
+
+num = int(input('Enter the value '))
+if num == 10:
+    print ('The number is 10')
+elif num == 20:
+    print ('The number is 20')
+elif num == 30:
+    print ('The number is 30')
+else:
+    print ('The number is above 30')'''
+
+# ====================================================================
+print('===================================')
+
+# Loops in python
+'''
+SYNTAX : 
+
+for iterating_var in sequence:  
+    statement(s)  
+'''
